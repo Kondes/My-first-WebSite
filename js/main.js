31b426c8b65b0ab208b86c318eb028a855a5652e
Andrii Kondysiuk <kondysiuka@gmail.com>
2020-11-16 02:27:54 +0000

    slick

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-$(function){
-    $('.photo__slider').slick({;
-});
+$(function () {
+  $('.photo__slider').slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="image/left.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="image/right.svg" alt=""></button>',
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
+  });
 });
